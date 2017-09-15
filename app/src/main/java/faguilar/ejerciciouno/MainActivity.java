c1e92051211ec899667737e88db41bf85a1932a2
Fernando Aguilar Villarroel <31317045+fernando-aguilar@users.noreply.github.com>
2017-09-15 10:39:42 -0400

    Mi primer Commint desde mi maquia de trabajo

--- app/src/main/java/faguilar/ejerciciouno/MainActivity.java
+++ app/src/main/java/faguilar/ejerciciouno/MainActivity.java
@@ -2,10 +2,12 @@ package faguilar.ejerciciouno;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView lblMensaje;
+    ImageView imgImagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         lblMensaje = (TextView) findViewById(R.id.lblMensaje);
         lblMensaje.setText("Hola Fernando !!");
+
+        imgImagen = (ImageView) findViewById(R.id.imgLogo);
     }
 }
